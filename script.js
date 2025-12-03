26f4c3f69da8bd77285e35052714f805fea0efe2
Tulio <221194504+tulin404@users.noreply.github.com>
2025-12-03 19:14:24 -0300

    FIFinished Nav animation and responsive design

--- script.js
+++ script.js
@@ -2,13 +2,12 @@
 const menu = document.getElementById('ham-menu');
 const nav = document.getElementById('nav');
 const smallHeader = document.getElementById('small-header-section');
+const bodyNoNav = document.querySelectorAll('body > *:not(nav):not(#ham-menu)');
 
 menu.addEventListener('click', () => {
-    if (smallHeader.contains(menu)) {
-        menu.classList.toggle('active');
-        nav.classList.toggle('active');
-    } else {
-        menu.classList.toggle('active');
-        nav.classList.toggle('active');
-    };
+    menu.classList.toggle('active');
+    nav.classList.toggle('active');
+    bodyNoNav.forEach(e => {
+        e.classList.toggle('opacity')
+    });
 })
